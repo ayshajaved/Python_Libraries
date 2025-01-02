f45a92d2909e1566085b7aa2ef455094b460ed14
ayshajaved <ayshajavd@gmail.com>
2025-01-02 20:14:17 +0500

    Plot Added with customization

--- Matplotlib/1_.py
+++ Matplotlib/1_.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as mt
 x = [1,2,3,4,5]
 y = [1,2,3,4,5]
 
-mt.plot(x,y)
+# mt.plot(x,y)
 mt.xlabel("x-axis")
 mt.ylabel("y-axis")
 
 mt.title("First plot")
 #showing a simple graph defining x axis label y axis label, the title of the graph
+#Now i will customize the plot
+
+mt.plot(x,y, color = "red", linestyle = "--", label = 'Data Line', marker = "o")
+mt.legend() #A legend is a box or area within the plot that provides labels for the elements in the graph, such as lines, markers, or other graphical elements. Each label explains what the corresponding element represents.
 mt.show()
