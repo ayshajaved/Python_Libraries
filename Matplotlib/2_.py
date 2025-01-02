cd511dbc578e9c0bd4755fd90f033c509013d7de
ayshajaved <ayshajavd@gmail.com>
2025-01-02 20:29:23 +0500

    Bar chart

---
+++ Matplotlib/2_.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plot
+#Implementing the bar chart plot
+categories = ['A', 'B', 'C', 'D']
+values = [5, 7, 3, 8]
+
+plot.bar(categories, values, color='green')
+plot.title("Bar Chart Example")
+plot.show()
